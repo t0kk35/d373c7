a51a057ecfe482cf19887635d0bf599266b36450
t0kk35 <tomsmaege@gmail.com>
2020-09-30 09:02:07 +0200

    [UPD] Commit first Feature

---
+++ d373c7/features/base.py
@@ -0,0 +1,61 @@
+"""
+Definition of some fairly straight forward Features
+(c) 2020 d373c7
+"""
+import logging
+from typing import List
+from d373c7.features.common import Feature, FeatureType
+logger = logging.getLogger(__name__)
+
+
+class FeatureDefinitionException(Exception):
+    """ Exception thrown when a the Definition of a feature fails
+    Args:
+        message: A free form text message describing the error
+    """
+    def __init__(self, message: str):
+        super().__init__("Error Defining Feature: " + message)
+
+
+def not_implemented(feature: Feature):
+    raise NotImplementedError(f'Feature problem. Not defined for class {feature.__class__.name}')
+
+
+class FeatureSource(Feature):
+    """" A feature found in a source. I.e a file or message or JSON or other. This is the most basic feature.
+    Args:
+        name: A name for the feature
+        f_type: The type of the feature. This must be an instance of the FeatureType class
+        format_code: A specific format to be applied to the input string. For instance to convert to a date.
+        default: A default value. If set, this value will be default if missing in the input source.
+    """
+    def __init__(self, name: str, f_type: FeatureType, format_code: str = None, default: any = None):
+        if type(name) != str:
+            raise FeatureDefinitionException('Source Field must have a name parameter of type string')
+        Feature.__init__(self, name, f_type)
+        self._format_code = format_code
+        self._default = default
+
+    def __eq__(self, other):
+        if isinstance(other, self.__class__):
+            return self.name == other.name
+        else:
+            return False
+
+    def __hash__(self):
+        return hash(self.name)
+
+    def __str__(self):
+        return f'Source Feature {self.name}'
+
+    @property
+    def embedded_features(self) -> List[Feature]:
+        return []
+
+    @property
+    def format_code(self):
+        return self._format_code
+
+    @property
+    def default(self) -> any:
+        return self._default
---
+++ d373c7/features/common.py
@@ -0,0 +1,107 @@
+"""
+Common classes for all features
+(c) 2020 d373c7
+"""
+from typing import List
+
+
+def not_implemented(class_):
+    raise NotImplementedError(f'Feature problem. Not defined for class {class_.__class__.name}')
+
+
+class FeatureType:
+    """ Defines the data type of a particular Feature. See below for the specific implementations"""
+    def __init__(self, key: int, name: str):
+        self._key = key
+        self._name = name
+
+    @property
+    def key(self) -> int:
+        return self._key
+
+    @property
+    def name(self) -> str:
+        return self._name
+
+    @property
+    def precision(self) -> int:
+        return not_implemented(self)
+
+    @staticmethod
+    def max_precision(ft1: 'FeatureType', ft2: 'FeatureType') -> 'FeatureType':
+        if ft1.precision > ft2.precision:
+            return ft1
+        else:
+            return ft2
+
+
+class FeatureTypeString(FeatureType):
+    pass
+
+
+class FeatureTypeNumerical(FeatureType):
+    def __init__(self, key: int, name: str, precision: int):
+        FeatureType.__init__(self, key, name)
+        self._precision = precision
+
+    @property
+    def precision(self) -> int:
+        return self._precision
+
+
+class FeatureTypeInteger(FeatureTypeNumerical):
+    pass
+
+
+class FeatureTypeFloat(FeatureTypeNumerical):
+    pass
+
+
+class FeatureTypeTimeBased(FeatureType):
+    pass
+
+
+class FeatureTypeBool(FeatureTypeNumerical):
+    pass
+
+
+FEATURE_TYPE_STRING: FeatureType = FeatureTypeString(1, 'STRING')
+FEATURE_TYPE_CATEGORICAL: FeatureType = FeatureTypeString(2, 'CATEGORICAL')
+FEATURE_TYPE_FLOAT: FeatureType = FeatureTypeFloat(3, 'FLOAT', 64)
+FEATURE_TYPE_FLOAT_32: FeatureType = FeatureTypeFloat(4, 'FLOAT_32', 32)
+FEATURE_TYPE_FLOAT_64: FeatureType = FEATURE_TYPE_FLOAT
+FEATURE_TYPE_DATE: FeatureType = FeatureTypeTimeBased(5, 'DATE')
+FEATURE_TYPE_DATE_TIME: FeatureType = FeatureTypeTimeBased(6, 'DATETIME')
+FEATURE_TYPE_INTEGER: FeatureType = FeatureTypeInteger(7, 'INTEGER', 32)
+FEATURE_TYPE_INT_8: FeatureType = FeatureTypeInteger(8, 'INT_8', 8)
+FEATURE_TYPE_INT_16: FeatureType = FeatureTypeInteger(9, 'INT_16', 16)
+FEATURE_TYPE_INT_32: FeatureType = FEATURE_TYPE_INTEGER
+FEATURE_TYPE_INT_64: FeatureType = FeatureTypeInteger(10, 'INT_64', 64)
+FEATURE_TYPE_BOOL: FeatureType = FeatureTypeBool(11, 'INT_8', 8)
+
+
+class Feature:
+    """ Base Feature class. All features will inherit from this class.
+    Args:
+        name: A name for the feature
+        f_type: The type of the feature. Must be a FeatureType class instance
+
+    Attributes:
+        name: The name for the feature
+        f_type: The type of the feature. Must be a FeatureType class instance
+    """
+    def __init__(self, name: str, f_type: FeatureType):
+        self.__name = name
+        self.__type = f_type
+
+    @property
+    def name(self) -> str:
+        return self.__name
+
+    @property
+    def type(self) -> FeatureType:
+        return self.__type
+
+    @property
+    def embedded_features(self) -> List['Feature']:
+        return not_implemented(self)
